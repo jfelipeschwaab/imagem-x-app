ba4bf96b73b50ab24f3c7f26002ae8591a04a2c2
Débora_Ferreira <deboracris29a@gmail.com>
2025-10-16 15:26:53 -0300

    feat: Criando branch  coreBluetooth

---
+++ ImagemX/CoreBluetooth/CoreBluetoothView.swift
@@ -0,0 +1,18 @@
+//
+//  CoreBluetoothView.swift
+//  ImagemX
+//
+//  Created by Débora Cristina Silva Ferreira on 16/10/25.
+//
+
+import SwiftUI
+
+struct CoreBluetoothView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoreBluetoothView()
+}
