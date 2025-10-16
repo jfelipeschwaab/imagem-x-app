51c95c045cf132b9274d20ad1f59203efcb0c738
jfelipeschwaab <joaofsp.games@gmail.com>
2025-10-16 14:34:06 -0300

    Initial Commit

---
+++ ImagemX/ImagemXApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagemXApp.swift
+//  ImagemX
+//
+//  Created by João Felipe Schwaab on 16/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ImagemXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
