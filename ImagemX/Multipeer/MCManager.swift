5690eb03bff3f963709976684ccbef1442be6923
VictorKaue <victorkaue909@gmail.com>
2025-10-17 14:59:38 -0300

    feature: create class MCManager

---
+++ ImagemX/Multipeer/MCManager.swift
@@ -0,0 +1,14 @@
+//
+//  MultipeerManager.swift
+//  ImagemX
+//
+//  Created by Victor Kaue Lima De Paiva on 16/10/25.
+//
+
+import Foundation
+import MultipeerConnectivity
+
+class MultipeerManager {
+    static var shared 
+    
+}
