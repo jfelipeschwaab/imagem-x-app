1ecbbb9d69c5c9d65eac09f44e5ee1011428a0c8
jfelipeschwaab <joaofsp.games@gmail.com>
2025-10-17 17:08:39 -0300

    adicionado container de cloudKit

--- ImagemX/FoundationsModelView.swift
+++ ImagemX/FoundationsModelView.swift
@@ -7,6 +7,15 @@
 
 import SwiftUI
 
+struct Message: Identifiable {
+    
+    var id = UUID()
+    var text: String
+    let isUser: Bool
+    
+}
+
+
 struct FoundationsModelView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
